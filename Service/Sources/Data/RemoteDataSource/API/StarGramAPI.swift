b20f43d515b97f448155055ef39a9c749a9f64eb
rlarldud1234 <rlarldud6482@naver.com>
2022-08-09 01:36:21 +0900

    ⚡️ :: StarGramAPI

--- Service/Sources/Data/RemoteDataSource/API/StarGramAPI.swift
+++ Service/Sources/Data/RemoteDataSource/API/StarGramAPI.swift
@@ -1,2 +1,40 @@
 import Foundation
 
+import Moya
+
+protocol StarGramAPI: TargetType {
+    var domain: ApiDomain { get }
+    var urlPath: String { get }
+}
+
+extension StarGramAPI {
+
+    var baseURL: URL { URL(string: "")! }
+
+    var path: String {
+        return domain.url+urlPath
+    }
+
+    var task: Task {
+        return .requestPlain
+    }
+
+    var headers: [String : String]? {
+        switch self {
+        default:
+            return ["Content-Type": "application/json"]
+        }
+    }
+}
+
+enum ApiDomain: String {
+    case auths
+    case feeds
+    case profiles
+}
+
+extension ApiDomain {
+    var url: String {
+        return "\(self.rawValue)"
+    }
+}
