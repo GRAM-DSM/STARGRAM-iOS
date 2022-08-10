69d2490fea4d9b91762005abe4d8a983ae3d60f2
rlarldud1234 <rlarldud6482@naver.com>
2022-08-10 15:50:46 +0900

    ⚡️ :: ImageResponse

---
+++ Service/Sources/Data/RemoteDataSource/Response/Feed/ImageResponse.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct ImageResponse: Decodable {
+    let older: Int
+    let image: String
+}
+
+extension ImageResponse {
+    func toDomain() -> Image {
+        return .init(
+            older: older,
+            image: image
+        )
+    }
+}
