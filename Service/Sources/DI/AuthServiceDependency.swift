eadf036c2ee6208b8ac66a8076aad185f8a9d690
rlarldud1234 <rlarldud6482@naver.com>
2022-08-11 22:24:25 +0900

    ⚡️ :: AuthServiceDependency

---
+++ Service/Sources/DI/AuthServiceDependency.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+public struct AuthServiceDependency {
+    public let checkIdUseCase: CheckIdUseCase
+    public let checkVerificationEmailUseCase: CheckVerificationEmailUseCase
+    public let refreshTokenUseCase: RefreshTokenUseCase
+    public let signInUseCase: SignInUseCase
+    public let signUpUseCase: SignUpUseCase
+    public let verificationEmailUseCase: VerificationEmailUseCase
+}
+
+public extension AuthServiceDependency {
+    static func resolve() -> AuthServiceDependency {
+
+        let repository: AuthRepository = AuthRepositoryImpl()
+
+        let checkIdUseCase = CheckIdUseCase(
+            repository: repository
+        )
+        let checkVerificationEmailUseCase = CheckVerificationEmailUseCase(
+            repository: repository
+        )
+        let refreshTokenUseCase = RefreshTokenUseCase(
+            repository: repository
+        )
+        let signInUseCase = SignInUseCase(
+            repository: repository
+        )
+        let signUpUseCase = SignUpUseCase(
+            repository: repository
+        )
+        let verificationEmailUseCase = VerificationEmailUseCase(
+            repository: repository
+        )
+
+        return AuthServiceDependency(
+            checkIdUseCase: checkIdUseCase,
+            checkVerificationEmailUseCase: checkVerificationEmailUseCase,
+            refreshTokenUseCase: refreshTokenUseCase,
+            signInUseCase: signInUseCase,
+            signUpUseCase: signUpUseCase,
+            verificationEmailUseCase: verificationEmailUseCase
+        )
+    }
+}
