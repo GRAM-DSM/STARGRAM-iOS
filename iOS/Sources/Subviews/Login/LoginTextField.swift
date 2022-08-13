6af6090786a7d8fa278da88332299adae5a39dec
rlarldud1234 <rlarldud6482@naver.com>
2022-08-14 01:19:14 +0900

    ⚡️ :: LoginTextField

--- iOS/Sources/Subviews/Login/LoginTextField.swift
+++ iOS/Sources/Subviews/Login/LoginTextField.swift
@@ -3,8 +3,32 @@ import SwiftUI
 struct LoginTextField: View {
     let placeholder: String
     @Binding var text: String
+    @Binding var entry: Bool
+
     var body: some View {
-        TextField(placeholder, text: $text)
+        HStack {
+            if entry {
+                SecureField(
+                    placeholder,
+                    text: $text
+                )
+                .padding(.horizontal, 15)
+                .font(.body200)
+            } else {
+                TextField(
+                    placeholder,
+                    text: $text
+                )
+                .padding(.horizontal, 15)
+                .font(.body200)
+            }
+        }
+        .frame(height: 51)
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(lineWidth: 1)
+                .foregroundColor(.gray1)
+        )
     }
 }
 
@@ -12,7 +36,8 @@ struct LoginTextField_Previews: PreviewProvider {
     static var previews: some View {
         LoginTextField(
             placeholder: "로그인",
-            text: .constant("")
+            text: .constant(""),
+            entry: .constant(false)
         )
     }
 }
