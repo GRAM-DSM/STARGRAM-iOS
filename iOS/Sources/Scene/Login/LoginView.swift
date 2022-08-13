5551b41fecd5c44e3f9c3e5c808a196334dbcf7f
rlarldud1234 <rlarldud6482@naver.com>
2022-08-14 01:18:22 +0900

    ⚡️ :: LoginView

--- iOS/Sources/Scene/Login/LoginView.swift
+++ iOS/Sources/Scene/Login/LoginView.swift
@@ -1,13 +1,39 @@
 import SwiftUI
 
 struct LoginView: View {
+    @StateObject var viewModel: LoginViewModel
+    @State var entry: Bool = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+                .frame(height: 150)
+            LoginTitle()
+            Spacer()
+                .frame(minHeight: 0, maxHeight: 120)
+            LoginTextFieldView(
+                id: $viewModel.id,
+                password: $viewModel.password
+            )
+            Spacer()
+                .frame(height: 40)
+            LoginButton(
+                action: {
+                    print("로그인")
+                },
+                isDisable: .constant(viewModel.id == "" || viewModel.password == "")
+            )
+            Spacer()
+                .frame(height: 14)
+            MoveSignUpButton()
+            Spacer()
+        }
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        let viewModel = LoginViewModel()
+        LoginView(viewModel: viewModel)
     }
 }
