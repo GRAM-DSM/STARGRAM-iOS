83f7fafb9ef4a5abf7203ab5823e6741f21ac51c
rlarldud1234 <rlarldud6482@naver.com>
2022-08-15 22:24:06 +0900

    ⚡️ :: HomeListCell

--- iOS/Sources/Scene/Home/Cell/HomeListCell.swift
+++ iOS/Sources/Scene/Home/Cell/HomeListCell.swift
@@ -5,8 +5,55 @@ import Service
 struct HomeListCell: View {
     var item: Feed
     var body: some View {
-        HStack {
-            
+        VStack {
+            Spacer()
+                .frame(height: 13)
+            HStack {
+                AsyncImage(url: item.imageUrl)
+                    .cornerRadius(8)
+                    .frame(width: 50, height: 50)
+                VStack(
+                    alignment: .leading,
+                    spacing: 0
+                ) {
+                    Text(item.title)
+                        .font(.button300)
+                    HStack {
+                        Text(item.name)
+                            .font(.small100)
+                        Text(item.created.toString())
+                            .font(.small100)
+                    }
+                }
+                Spacer()
+                VStack {
+                    Button {
+                        print("!")
+                    } label: {
+                        Image(systemName: item.heartStatus ? "heart.fill" : "heart")
+                            .foregroundColor(.orange1)
+                    }
+                    Text("\(item.heartCount)")
+                        .font(.small200)
+                }
+                VStack {
+                    Button {
+                        print("comment")
+                    } label: {
+                        Image(systemName: "bubble.left")
+                            .foregroundColor(.orange1)
+                    }
+                    Text("\(item.commentCount)")
+                        .font(.small200)
+                }
+            }
+            Color.gray2
+                .frame(
+                    maxWidth: .infinity,
+                    maxHeight: 1
+                )
+            Spacer()
+                .frame(height: 13)
         }
     }
 }
@@ -21,7 +68,7 @@ struct HomeListCell_Previews: PreviewProvider {
                 created: Date(),
                 heartCount: 9,
                 commentCount: 2,
-                imageUrl: URL(string: "http://newsimg.hankookilbo.com/2017/11/06/201711060912547465_1.jpg")!,
+                imageUrl: URL(string: "https://www.remove.bg/ko")!,
                 heartStatus: false
             )
         )
