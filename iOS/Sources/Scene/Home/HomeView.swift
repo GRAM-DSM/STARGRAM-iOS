ab8bd94ccba7369bb2d20591e754017f9f8004eb
rlarldud1234 <rlarldud6482@naver.com>
2022-08-15 22:23:41 +0900

    ⚡️ :: HomeView

--- iOS/Sources/Scene/Home/HomeView.swift
+++ iOS/Sources/Scene/Home/HomeView.swift
@@ -1,17 +1,24 @@
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject var viewModel: HomeViewModel
     var body: some View {
         VStack(spacing: 3) {
+            Spacer()
+                .frame(height: 24)
             HomeTitle()
             Image("BackImage")
                 .padding(.horizontal, 16)
+            List(viewModel.feeds, id: \.id) { feed in
+                HomeListCell(item: feed)
+            }.padding(.horizontal, 16)
         }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        let viewModel = HomeViewModel()
+        HomeView(viewModel: viewModel)
     }
 }
