31e4fa3a3ea76fb6bd95fd13292c912837aeb73b
rlarldud1234 <rlarldud6482@naver.com>
2022-08-15 21:17:04 +0900

    ⚡️ :: HomeListCell 추가하기

---
+++ iOS/Sources/Scene/Home/Cell/HomeListCell.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+import Service
+
+struct HomeListCell: View {
+    var item: Feed
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
+
+struct HomeListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeListCell()
+    }
+}
