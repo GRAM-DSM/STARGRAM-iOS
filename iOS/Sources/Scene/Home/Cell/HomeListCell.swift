af79e1b147b55502c8271eea4646fd055511c978
rlarldud1234 <rlarldud6482@naver.com>
2022-08-15 21:35:37 +0900

    ⚡️ :: HomeListcell Feed값 넣어주기

--- iOS/Sources/Scene/Home/Cell/HomeListCell.swift
+++ iOS/Sources/Scene/Home/Cell/HomeListCell.swift
@@ -13,6 +13,17 @@ struct HomeListCell: View {
 
 struct HomeListCell_Previews: PreviewProvider {
     static var previews: some View {
-        HomeListCell()
+        HomeListCell(
+            item: Feed(
+                id: "",
+                name: "김기영",
+                title: "울 영웅이 졸귀",
+                created: Date(),
+                heartCount: 9,
+                commentCount: 2,
+                imageUrl: URL(string: "http://newsimg.hankookilbo.com/2017/11/06/201711060912547465_1.jpg")!,
+                heartStatus: false
+            )
+        )
     }
 }
