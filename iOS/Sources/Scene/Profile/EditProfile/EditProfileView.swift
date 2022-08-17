b553d1c063067bfca3d06ac49e04f4e65c340730
rlarldud1234 <rlarldud6482@naver.com>
2022-08-17 15:23:38 +0900

    ⚡️ :: EditProfileView

---
+++ iOS/Sources/Scene/Profile/EditProfile/EditProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  EditProfileView.swift
+//  STARGRAM-iOS
+//
+//  Created by 김기영 on 2022/08/17.
+//  Copyright © 2022 com.GRAM-DSM. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
