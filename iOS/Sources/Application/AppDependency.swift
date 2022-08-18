3d8053a37193d23f91fdd7dc7b3e03d7a60e9dcd
rlarldud1234 <rlarldud6482@naver.com>
2022-08-18 22:12:57 +0900

    ⚡️ :: AppDependency

--- iOS/Sources/Application/AppDependency.swift
+++ iOS/Sources/Application/AppDependency.swift
@@ -2,17 +2,20 @@ import Foundation
 
 struct AppDependency {
     let mainView: MainView
+    let editProfileView: EditProfileView
 }
 
 extension AppDependency {
     static func resolve() -> AppDependency {
         let homeViewModel = HomeViewModel()
         let profileViewModel = ProfileViewModel()
+        let editProfileViewModel = EditProfileViewModel()
 
         let homeView = HomeView(viewModel: homeViewModel)
         let searchView = SearchView()
         let writeView = WriteView()
         let profileView = ProfileView(viewModel: profileViewModel)
+        let editProfileView = EditProfileView(viewModel: editProfileViewModel)
 
         let mainView = MainView(
             homeView: homeView,
@@ -21,6 +24,9 @@ extension AppDependency {
             profileView: profileView
             )
 
-        return AppDependency(mainView: mainView)
+        return AppDependency(
+            mainView: mainView,
+            editProfileView: editProfileView
+        )
     }
 }
