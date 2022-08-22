02dcfec0454fc7a33b90b646628e1460b42aeafc
rlarldud1234 <rlarldud6482@naver.com>
2022-08-22 13:58:48 +0900

    ⚰️ :: 파일 이동으로 인한 코드 삭제

--- iOS/Sources/Util/Date + toString.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Date {
-    func toString() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yy.MM.dd"
-        return dateFormatter.string(from: self)
-    }
-}
