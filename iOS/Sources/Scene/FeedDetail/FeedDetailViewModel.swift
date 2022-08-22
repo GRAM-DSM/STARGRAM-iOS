ae9e1626334ec85db63b08c115aaf84b37f9a413
rlarldud1234 <rlarldud6482@naver.com>
2022-08-22 14:00:07 +0900

    ⚡️ :: FeedDetailViewModel

--- iOS/Sources/Scene/FeedDetail/FeedDetailViewModel.swift
+++ iOS/Sources/Scene/FeedDetail/FeedDetailViewModel.swift
@@ -3,13 +3,14 @@ import Foundation
 import Service
 
 class FeedDetailViewModel: ObservableObject {
+    @Published var comment: String = ""
     @Published var heartIsClick: Bool = false
     @Published var feedDetail: FeedDetail = .init(
         id: "",
         profileImageUrl: "",
-        name: "",
-        title: "",
-        content: "",
+        name: "토끼",
+        title: "토끼",
+        content: "토끼는 사람이 아닙니다.",
         created: Date(),
         category: "iOS",
         heartCount: 3,
