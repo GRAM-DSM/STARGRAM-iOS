242dfbe27c9b49dda85d0764bf62900af0cf52f1
rlarldud1234 <rlarldud6482@naver.com>
2022-08-28 21:19:13 +0900

    ⚡️ :: DropDown Button

---
+++ iOS/Sources/Components/DropDown/DropDownButton.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct DropDownButton: View {
+    @Binding var isPressend: Bool
+    var major: String
+    var body: some View {
+        HStack(
+            alignment: .center,
+            spacing: 0
+        ) {
+            Image(systemName: isPressend ? "checkmark" : "")
+                .resizable()
+                .frame(width: 8, height: 10)
+                .foregroundColor(.gray1)
+                .padding(.horizontal, 7)
+            Text(major)
+                .font(.small100)
+                .foregroundColor(.black)
+            Spacer()
+        }
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: 38
+        )
+        .border(
+            width: 1,
+            edges: [.bottom],
+            color: .gray2
+        )
+    }
+}
+
+struct DropDownButton_Previews: PreviewProvider {
+    static var previews: some View {
+        DropDownButton(
+            isPressend: .constant(true),
+            major: "Backend"
+        )
+    }
+}
