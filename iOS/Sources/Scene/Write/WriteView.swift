34e20d27b83fa2eed61cec00a80cb68b93cf9a2d
rlarldud1234 <rlarldud6482@naver.com>
2022-09-03 15:45:10 +0900

    ⚡️ :: WrtieView

--- iOS/Sources/Scene/Write/WriteView.swift
+++ iOS/Sources/Scene/Write/WriteView.swift
@@ -1,13 +1,86 @@
 import SwiftUI
 
+import PhotosUI
+
 struct WriteView: View {
+    @StateObject var viewModel: WriteViewModel
+    @State var showPhotoPicker: Bool = false
     var body: some View {
-        Text("게시물 작성 화면입니다.")
+        NavigationView {
+            VStack {
+                Spacer()
+                    .frame(height: 30)
+                DropdownSelector(
+                    selectedOption: $viewModel.major,
+                    placeholder: "분야 선택"
+                )
+                .background(
+                    VStack(spacing: 0) {
+                        Spacer()
+                            .frame(height: 60)
+                        TextField(
+                            "제목을 입력해주세요.",
+                            text: $viewModel.title
+                        )
+                        .font(.body300)
+                        .padding(.vertical, 6)
+                        .border(
+                            width: 1,
+                            edges: [.bottom],
+                            color: .gray2
+                        )
+                        .padding(.horizontal, 33)
+
+                        Spacer()
+                            .frame(height: 28)
+
+                        PostImageView(
+                            images: $viewModel.images,
+                            showImagePicker: $showPhotoPicker
+                        )
+                        .padding(.horizontal, 33)
+
+                        Spacer()
+                            .frame(height: 13)
+
+                        ZStack(alignment: .topLeading) {
+                            TextEditor(text: $viewModel.content)
+                                .font(.body500)
+                                .padding(.horizontal, 30)
+                                .frame(
+                                    minHeight: 340,
+                                    maxHeight: .infinity
+                                )
+                            if viewModel.content.isEmpty {
+                                Text("내용을 입력하세요.")
+                                    .font(.body500)
+                                    .foregroundColor(.gray1)
+                                    .padding(.leading, 33)
+                            }
+                        }
+                    },
+                    alignment: .top
+                )
+                Spacer()
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                Button {
+                    print("게시")
+                } label: {
+                    Text("게시")
+                        .foregroundColor(.orange1)
+                        .font(.body700)
+                        .padding()
+                }
+            }
+        }
     }
 }
 
 struct WriteView_Previews: PreviewProvider {
     static var previews: some View {
-        WriteView()
+        let viewModel = WriteViewModel()
+        WriteView(viewModel: viewModel)
     }
 }
