a8eef0ebdbe29acd06c0dec4a3e63bcf34945093
rlarldud1234 <rlarldud6482@naver.com>
2022-09-03 15:45:02 +0900

    ⚡️ :: WrtieViewModel

--- iOS/Sources/Scene/Write/WriteViewModel.swift
+++ iOS/Sources/Scene/Write/WriteViewModel.swift
@@ -1,7 +1,10 @@
 import Foundation
 
+import UIKit
+
 class WriteViewModel: ObservableObject {
     @Published var major: String = ""
     @Published var title: String = ""
+    @Published var images: [UIImage] = []
     @Published var content: String = ""
 }
