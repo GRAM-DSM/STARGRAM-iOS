4b8c587276bc44c68dff5aee77653730e71a2415
rlarldud1234 <rlarldud6482@naver.com>
2022-09-21 15:18:31 +0900

    ⚡️ :: SPM 삭제

--- Tuist/ProjectDescriptionHelpers/TargetDependency+SPM.swift
+++ Tuist/ProjectDescriptionHelpers/TargetDependency+SPM.swift
@@ -2,29 +2,9 @@ import ProjectDescription
 
 extension TargetDependency {
     public struct SPM {
-        public static let RxSwift = TargetDependency.package(product: "RxSwift")
-        public static let Moya = TargetDependency.package(product: "Moya")
-        public static let KeychainSwift = TargetDependency.package(product: "KeychainSwift")
-        public static let RxMoya = TargetDependency.package(product: "RxMoya")
+        public static let RxSwift = TargetDependency.external(name: "RxSwift")
+        public static let Moya = TargetDependency.external(name: "Moya")
+        public static let KeychainSwift = TargetDependency.external(name: "KeychainSwift")
+        public static let RxMoya = TargetDependency.external(name: "RxMoya")
     }
-}
-
-public extension Package {
-
-    // RxSwift
-    static let RxSwift = Package.remote(
-        url: "https://github.com/ReactiveX/RxSwift",
-        requirement: .upToNextMajor(from: "6.5.0")
-    )
-    // Moya
-    static let Moya = Package.remote(
-        url: "https://github.com/Moya/Moya.git",
-        requirement: .upToNextMajor(from: "15.0.0")
-    )
-    // KeychainSwift
-    static let KeychainSwift = Package.remote(
-        url: "https://github.com/evgenyneu/keychain-swift",
-        requirement: .upToNextMajor(from: "20.0.0")
-    )
-
 }
