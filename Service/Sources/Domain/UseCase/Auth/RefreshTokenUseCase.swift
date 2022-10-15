70e00c8e656a38531871be4c605bf4de29de91e5
rlarldud1234 <rlarldud6482@naver.com>
2022-10-15 14:57:37 +0900

    ⚡️ :: RefreshTokenUseCase

--- Service/Sources/Domain/UseCase/Auth/RefreshTokenUseCase.swift
+++ Service/Sources/Domain/UseCase/Auth/RefreshTokenUseCase.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-import RxSwift
+import Combine
 
 public class RefreshTokenUseCase {
 
@@ -10,7 +10,7 @@ public class RefreshTokenUseCase {
         self.repository = repository
     }
 
-    public func excute() -> Completable {
+    public func excute() -> AnyPublisher<Void, STARGRAMError> {
         return repository.refreshToken()
     }
 }
