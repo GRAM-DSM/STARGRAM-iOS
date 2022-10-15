ad6969143955f57cf0080f901b28c9f4e1350d61
rlarldud1234 <rlarldud6482@naver.com>
2022-10-15 15:00:35 +0900

    ⚡️ :: DeleteProfileImageUseCase

--- Service/Sources/Domain/UseCase/Profile/DeleteProfileImageUseCase.swift
+++ Service/Sources/Domain/UseCase/Profile/DeleteProfileImageUseCase.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-import RxSwift
+import Combine
 
 public class DeleteProfileImageUseCase {
 
@@ -10,7 +10,7 @@ public class DeleteProfileImageUseCase {
         self.repository = repository
     }
 
-    public func excute() -> Completable {
+    public func excute() -> AnyPublisher<Void, STARGRAMError> {
         return repository.deleteProfileImage()
     }
 }
