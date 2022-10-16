5972bac714569f7591d3b98e613f49bea5562254
rlarldud1234 <rlarldud6482@naver.com>
2022-10-16 16:35:20 +0900

    ⚡️ :: 로그인 기능 구현

--- iOS/Sources/Scene/Login/LoginViewModel.swift
+++ iOS/Sources/Scene/Login/LoginViewModel.swift
@@ -1,6 +1,34 @@
 import Foundation
 
+import Combine
+import Service
+
 class LoginViewModel: ObservableObject {
     @Published var id: String = ""
     @Published var password: String = ""
+    @Published var isSuccess: Bool = false
+    @Published var isDisabled: Bool = true
+
+    private let signInUseCase: SignInUseCase
+
+    init(signInUseCase: SignInUseCase) {
+        self.signInUseCase = signInUseCase
+    }
+
+    private var bag = Set<AnyCancellable>()
+
+    func login() {
+        signInUseCase.excute(id: id, password: password)
+            .sink { error in
+                print(error.hashValue)
+                self.isSuccess = false
+            } receiveValue: { _ in
+                self.isSuccess = true
+            }
+            .store(in: &bag)
+    }
+
+    func isEmptyTextFields() {
+        isDisabled = id.isEmpty || password.isEmpty
+    }
 }
