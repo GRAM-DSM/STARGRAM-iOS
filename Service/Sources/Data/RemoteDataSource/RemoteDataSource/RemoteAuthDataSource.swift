c44a77b691e6e6d586f39fe400c5bae7ed0ac942
rlarldud1234 <rlarldud6482@naver.com>
2022-10-18 18:51:40 +0900

    ⚡️ :: 타입 변경하기

--- Service/Sources/Data/RemoteDataSource/RemoteDataSource/RemoteAuthDataSource.swift
+++ Service/Sources/Data/RemoteDataSource/RemoteDataSource/RemoteAuthDataSource.swift
@@ -10,29 +10,29 @@ final class RemoteAuthDataSource: MoyaProvider<AuthAPI> {
 
     private init() { }
 
-    func signin(_ request: SigninRequest) -> AnyPublisher<TokenResponse, STARGRAMError> {
+    func signin(_ request: SigninRequest) -> AnyPublisher<TokenResponse, MoyaError> {
         return self.requestPublisher(.signin(request), TokenResponse.self)
     }
 
-    func signup(_ request: SignupRequest) -> AnyPublisher<Void, STARGRAMError> {
+    func signup(_ request: SignupRequest) -> AnyPublisher<Void, MoyaError> {
         return self.requestVoidPublisher(.signup(request))
     }
 
-    func checkId(_ id: String) -> AnyPublisher<Void, STARGRAMError> {
+    func checkId(_ id: String) -> AnyPublisher<Void, MoyaError> {
         return self.requestVoidPublisher(.checkId(id))
     }
 
-    func refreshToken(_ refreshToken: String) -> AnyPublisher<TokenResponse, STARGRAMError> {
+    func refreshToken(_ refreshToken: String) -> AnyPublisher<TokenResponse, MoyaError> {
         return self.requestPublisher(.refreshToken(refreshToken), TokenResponse.self)
     }
 
-    func verificationEmail(_ email: String) -> AnyPublisher<Void, STARGRAMError> {
+    func verificationEmail(_ email: String) -> AnyPublisher<Void, MoyaError> {
         return self.requestVoidPublisher(.verificationEmail(email))
     }
 
     func checkVerficationEmail(
         _ request: CheckVerificationEmailRequest
-    ) -> AnyPublisher<Void, STARGRAMError> {
+    ) -> AnyPublisher<Void, MoyaError> {
         return self.requestVoidPublisher(.checkVerificationEmail(request))
     }
 }
