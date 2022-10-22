b06e74b1d50779db0da7a2f0ccf1c1f50359c4d9
rlarldud1234 <rlarldud6482@naver.com>
2022-10-22 23:22:54 +0900

    ⚡️ :: UIKit 추가

--- iOS/Sources/Scene/Profile/EditProfile/EditProfileViewModel.swift
+++ iOS/Sources/Scene/Profile/EditProfile/EditProfileViewModel.swift
@@ -1,7 +1,10 @@
 import Foundation
 
+import UIKit
+
 class EditProfileViewModel: ObservableObject {
     @Published var name: String = ""
     @Published var introduce: String = ""
     @Published var link: String = ""
+    @Published var image: UIImage = UIImage()
 }
