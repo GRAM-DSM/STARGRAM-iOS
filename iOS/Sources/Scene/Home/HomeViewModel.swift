0e26196a24ddaf774584ad1d8b6340cc86aec8eb
rlarldud1234 <rlarldud6482@naver.com>
2022-10-22 23:18:09 +0900

    ⚡️ :: HomeViewModel

--- iOS/Sources/Scene/Home/HomeViewModel.swift
+++ iOS/Sources/Scene/Home/HomeViewModel.swift
@@ -1,7 +1,31 @@
 import Foundation
 
+import Combine
 import Service
 
 class HomeViewModel: ObservableObject {
     @Published var feeds: [Feed] = []
+    @Published var isNotProfile: Bool = false
+
+    private let fetchProfileUseCase: FetchProfileUseCase
+
+    init(fetchProfileUseCase: FetchProfileUseCase) {
+        self.fetchProfileUseCase = fetchProfileUseCase
+    }
+
+    private var bag = Set<AnyCancellable>()
+
+    func checkUserHaveProfile() {
+        self.fetchProfileUseCase.excute()
+            .catch { error -> Empty<Profile, Never> in
+                if error == .notFound {
+                    self.isNotProfile = true
+                }
+                return .init()
+            }
+            .sink { _ in
+                self.isNotProfile = false
+            }
+            .store(in: &bag)
+    }
 }
