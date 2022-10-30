023e8a274e2eac69ed65cf7ad6efc35610fea453
rlarldud1234 <rlarldud6482@naver.com>
2022-10-30 18:59:43 +0900

    ⚡️ :: toImage 코드 변경

---
+++ iOS/Sources/Extension/String+toImage.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+import UIKit
+
+extension String {
+    func toImage() -> UIImage {
+        if let data = Data(base64Encoded: self, options: .ignoreUnknownCharacters) {
+            return UIImage(data: data) ?? UIImage()
+        }
+        return UIImage()
+    }
+}
