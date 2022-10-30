ff111aa7e591e6ea0174cb0dda5545f8e998bf52
rlarldud1234 <rlarldud6482@naver.com>
2022-10-30 16:14:49 +0900

    ⚡️ :: HomeViewModel

--- iOS/Sources/Scene/Home/HomeViewModel.swift
+++ iOS/Sources/Scene/Home/HomeViewModel.swift
@@ -1,7 +1,28 @@
 import Foundation
 
+import Combine
 import Service
 
 class HomeViewModel: ObservableObject {
     @Published var feeds: [Feed] = []
+
+    private let fetchFeedsUseCase: FetchFeedsUseCase
+
+    init(fetchFeedsUseCase: FetchFeedsUseCase) {
+        self.fetchFeedsUseCase = fetchFeedsUseCase
+    }
+
+    private var bag = Set<AnyCancellable>()
+
+    func fetchFeeds() {
+        self.fetchFeedsUseCase.excute()
+            .catch { error -> Empty<[Feed], Never> in
+                print(error)
+                return .init()
+            }
+            .sink {
+                self.feeds = $0
+            }
+            .store(in: &bag)
+    }
 }
