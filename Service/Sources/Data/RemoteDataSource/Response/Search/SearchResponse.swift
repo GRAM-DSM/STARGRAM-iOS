59afcc3de0303f9147b675043a25dc4dc6e22c61
rlarldud1234 <rlarldud6482@naver.com>
2022-11-27 23:28:35 +0900

    ⚡️ :: 리스폰스 이름만 변경하기

--- Service/Sources/Data/RemoteDataSource/Response/Search/SearchResponse.swift
+++ Service/Sources/Data/RemoteDataSource/Response/Search/SearchResponse.swift
@@ -4,28 +4,16 @@ struct SearchResponse: Decodable {
     private enum CodingKeys: String, CodingKey {
         case feedId = "feed_uuid"
         case title
-        case createAt = "create_at"
-        case heartCount = "heart_count"
-        case commentCount = "comment_count"
-        case image
     }
     let feedId: String
     let title: String
-    let createAt: String
-    let heartCount: Int
-    let commentCount: Int
-    let image: String
 }
 
 extension SearchResponse {
     func toDomain() -> Search {
         return .init(
             feedId: feedId,
-            title: title,
-            createAt: createAt.toDate(),
-            heartCount: heartCount,
-            commentCount: commentCount,
-            image: image
+            title: title
         )
     }
 }
--- Service/Sources/Domain/Entity/Search/Search.swift
+++ Service/Sources/Domain/Entity/Search/Search.swift
@@ -3,8 +3,4 @@ import Foundation
 public struct Search: Equatable {
     public let feedId: String
     public let title: String
-    public let createAt: Date
-    public let heartCount: Int
-    public let commentCount: Int
-    public let image: String
 }
