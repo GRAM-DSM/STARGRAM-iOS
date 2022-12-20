10a1b81ef28df18f0b1e735179f6f9f960901819
rlarldud1234 <rlarldud6482@naver.com>
2022-12-20 20:23:28 +0900

    ⚡️ :: 초기화 및 id를 받아올 수 있도록 구현

--- iOS/Sources/Scene/FeedDetail/FeedDetailViewModel.swift
+++ iOS/Sources/Scene/FeedDetail/FeedDetailViewModel.swift
@@ -1,22 +1,47 @@
 import Foundation
 
+import Combine
 import Service
 
 class FeedDetailViewModel: ObservableObject {
+    @Published var id: String = ""
     @Published var comment: String = ""
     @Published var heartIsClick: Bool = false
     @Published var feedDetail: FeedDetail = .init(
         id: "",
         profileImageUrl: "",
-        name: "토끼",
-        title: "토끼",
-        content: "토끼는 사람이 아닙니다.",
+        name: "",
+        title: "",
+        content: "",
         created: Date(),
-        category: "iOS",
-        heartCount: 3,
-        commentCount: 3,
+        category: "",
+        heartCount: 0,
+        commentCount: 0,
         favorite: false,
-        images: ["sdfsdfsd"],
+        images: [""],
         comments: []
     )
+
+    private let fetchFeedDetailUseCase: FetchFeedDetailUseCase
+
+    init(fetchFeedDetailUseCase: FetchFeedDetailUseCase) {
+        self.fetchFeedDetailUseCase = fetchFeedDetailUseCase
+    }
+
+    private var bag = Set<AnyCancellable>()
+
+    func fetchFeedDetail() {
+        self.fetchFeedDetailUseCase.excute(feedId: id)
+            .catch { error -> Empty<FeedDetail, Never> in
+                print(error)
+                return .init()
+            }
+            .sink { [weak self] feedDetail in
+                self?.feedDetail = feedDetail
+            }
+            .store(in: &bag)
+    }
+
+    func like() {
+    }
 }
