1e2c651704ad13b7d448d4d946352a4e74abd14d
rlarldud1234 <rlarldud6482@naver.com>
2022-12-22 23:38:13 +0900

    ⚡️ :: MainView

--- iOS/Sources/Scene/Main/MainView.swift
+++ iOS/Sources/Scene/Main/MainView.swift
@@ -1,33 +1,57 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var index: Int = 0
+    @State private var tabbarHidden: Bool = false
+    private let tabImage = ["house", "magnifyingglass", "pencil", "person"]
+    private let tabFillImage = ["house.circle.fill", "magnifyingglass.circle.fill",
+                                "pencil.circle.fill", "person.circle.fill"]
     let homeView: HomeView
     let searchView: SearchView
     let writeView: WriteView
     let profileView: ProfileView
 
     var body: some View {
-        TabView {
-            homeView
-                .tabItem {
-                    Image("HomeImage")
+        ZStack {
+            switch index {
+            case 0:
+                homeView
+            case 1:
+                searchView
+            case 2:
+                writeView
+            default:
+                profileView
+            }
+            if !tabbarHidden {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        ForEach(0..<tabImage.count, id: \.self) { index in
+                            Image(systemName: self.index == index ? tabFillImage[index] : tabImage[index])
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .foregroundColor(self.index == index ? .orange1 : .gray)
+                                .onTapGesture {
+                                    self.index = index
+                                }
+                            Spacer()
+                        }
+                    }
+                    .frame(height: 50)
+                    .padding(.top, 5)
+                    .padding(.bottom, 25)
+                    .background(Color.white)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray1, lineWidth: 0.2)
+                    )
                 }
-            searchView
-                .tabItem {
-                    Image("SearchImage")
-                }
-            writeView
-                .tabItem {
-                    Image("WriteImage")
-                }
-            profileView
-                .tabItem {
-                    Image("ProfileImage")
-                }
+                .ignoresSafeArea()
+            }
         }
-        .onAppear {
-            UITabBar.appearance().backgroundColor = .white
-        }
-        .navigationBarBackButtonHidden()
+        .environment(\.tabbarHidden, $tabbarHidden)
+        .ignoresSafeArea()
     }
 }
