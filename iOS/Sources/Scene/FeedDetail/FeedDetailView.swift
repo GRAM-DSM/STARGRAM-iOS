ebfa10fe70f800d671db51249a96daab15bae244
rlarldud1234 <rlarldud6482@naver.com>
2022-12-22 23:37:33 +0900

    ⚡️ :: FeedDetail

--- iOS/Sources/Scene/FeedDetail/FeedDetailView.swift
+++ iOS/Sources/Scene/FeedDetail/FeedDetailView.swift
@@ -3,124 +3,123 @@ import SwiftUI
 struct FeedDetailView: View {
     @StateObject var viewModel: FeedDetailViewModel
     @Environment(\.dismiss) var dismiss
+    @Environment(\.tabbarHidden) var tabbarHidden
 
     var body: some View {
-        VStack(
-            alignment: .leading,
-            spacing: 0
-        ) {
-            ScrollView {
-                Spacer()
-                    .frame(height: 15)
-                HStack(spacing: 1) {
-                    Text("Backend")
-                        .font(.small100)
-                        .padding(.leading, 16)
-                    Image(systemName: "chevron.right")
-                        .resizable()
-                        .foregroundColor(.orange1)
-                        .frame(width: 10, height: 10)
+            VStack(
+                alignment: .leading,
+                spacing: 0
+            ) {
+                ScrollView {
                     Spacer()
-                }
-                HStack {
-                    Text(viewModel.feedDetail.title)
-                        .font(.title500)
-                        .padding(.top, 13)
-                        .padding(.bottom, 11)
-                        .padding(.leading, 16)
-                    Spacer()
-                }
-                WriterView(
-                    image: viewModel.feedDetail.profileImageUrl,
-                    name: viewModel.feedDetail.name,
-                    created: Date(),
-                    isPressed: .constant(false)
-                )
-                .padding(.bottom, 19)
-                ZStack(alignment: .bottom) {
-                    FeedDetailTabView(images: viewModel.feedDetail.images)
-                        .frame(height: 290)
-                    HStack(
-                        alignment: .center,
-                        spacing: 0
-                    ) {
-                        Spacer()
-                            .frame(width: 28)
-                        Button {
-                            viewModel.heartIsClick.toggle()
-                        } label: {
-                            Image(systemName: viewModel.heartIsClick ? "heart.fill" : "heart")
-                                .foregroundColor(.orange1)
-                        }
-                        Spacer()
-                            .frame(width: 7)
-                        Text("\(viewModel.feedDetail.heartCount)")
-                            .font(.button500)
-                            .padding(.trailing, 18)
-                        Image(systemName: "bubble.left")
+                        .frame(height: 15)
+                    HStack(spacing: 1) {
+                        Text(viewModel.feedDetail.category)
+                            .font(.small100)
+                            .padding(.leading, 16)
+                        Image(systemName: "chevron.right")
+                            .resizable()
                             .foregroundColor(.orange1)
-                        Spacer()
-                            .frame(width: 7)
-                        Text("\(viewModel.feedDetail.commentCount)")
-                            .font(.button500)
+                            .frame(width: 10, height: 10)
                         Spacer()
                     }
-                    .padding(.bottom, 14)
-                }
-                TextEditor(text: $viewModel.feedDetail.content)
-                    .disabled(true)
-                    .font(.body400)
-                    .padding(.horizontal, 16)
-                FeedDetailTextField(
-                    action: {
-                        // action
-                    }, text: $viewModel.comment
-                )
-                .ignoresSafeArea(.keyboard)
-                Color.gray2
-                    .frame(
-                        maxWidth: .infinity,
-                        maxHeight: 1
+                    HStack {
+                        Text(viewModel.feedDetail.title)
+                            .font(.title500)
+                            .padding(.top, 13)
+                            .padding(.bottom, 11)
+                            .padding(.leading, 16)
+                        Spacer()
+                    }
+                    WriterView(
+                        image: viewModel.feedDetail.profileImageUrl,
+                        name: viewModel.feedDetail.name,
+                        created: Date(),
+                        action: viewModel.bookmark,
+                        isPressed: $viewModel.feedDetail.favorite
                     )
-                HStack {
-                    Text("댓글 \(viewModel.feedDetail.comments.count)개")
+                    .padding(.bottom, 19)
+                    ZStack(alignment: .bottom) {
+                        FeedDetailTabView(images: viewModel.feedDetail.images)
+                            .frame(height: 290)
+                        HStack(
+                            alignment: .center,
+                            spacing: 0
+                        ) {
+                            Spacer()
+                                .frame(width: 28)
+                            Button(action: viewModel.like) {
+                                Image(systemName: viewModel.feedDetail.heartState ? "heart.fill" : "heart")
+                                    .foregroundColor(.orange1)
+                            }
+                            Spacer()
+                                .frame(width: 7)
+                            Text("\(viewModel.feedDetail.heartCount)")
+                                .font(.button500)
+                                .padding(.trailing, 18)
+                            Image(systemName: "bubble.left")
+                                .foregroundColor(.orange1)
+                            Spacer()
+                                .frame(width: 7)
+                            Text("\(viewModel.feedDetail.commentCount)")
+                                .font(.button500)
+                            Spacer()
+                        }
+                        .padding(.bottom, 14)
+                    }
+                    TextEditor(text: $viewModel.feedDetail.content)
+                        .disabled(true)
                         .font(.body400)
-                        .padding(.leading, 16)
-                    Spacer()
-                }
-                ForEach(viewModel.feedDetail.comments, id: \.content) {
-                    CommentListCell(comment: $0)
-                }
-            }
-        }
-        .onChange(of: viewModel.id, perform: { _ in
-            self.viewModel.fetchFeedDetail()
-        })
-        .onAppear {
-            UITabBar.appearance().isHidden = true
-        }
-        .toolbar(content: {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "chevron.backward")
-                        .resizable()
-                        .foregroundColor(.white)
-                        .frame(width: 10, height: 15)
+                        .padding(.horizontal, 16)
+                    FeedDetailTextField(
+                        action: {
+                            // action
+                        }, text: $viewModel.comment
+                    )
+                    .ignoresSafeArea(.keyboard)
+                    Color.gray2
+                        .frame(
+                            maxWidth: .infinity,
+                            maxHeight: 1
+                        )
+                    HStack {
+                        Text("댓글 \(viewModel.feedDetail.comments.count)개")
+                            .font(.body400)
+                            .padding(.leading, 16)
+                        Spacer()
+                    }
+                    ForEach(viewModel.feedDetail.comments, id: \.content) {
+                        CommentListCell(comment: $0)
+                    }
                 }
             }
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button {
-                } label: {
-                    Image(systemName: "ellipsis")
-                        .resizable()
-                        .foregroundColor(.white)
-                        .frame(width: 15)
+            .onChange(of: viewModel.id, perform: { _ in
+                self.viewModel.fetchFeedDetail()
+            })
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                            .resizable()
+                            .foregroundColor(.white)
+                            .frame(width: 10, height: 15)
+                    }
                 }
-            }
-        })
-        .navigationBarColor(.orange1, textColor: .white)
-        .navigationBarBackButtonHidden()
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                    } label: {
+                        Image(systemName: "ellipsis")
+                            .resizable()
+                            .foregroundColor(.white)
+                            .frame(width: 15)
+                    }
+                }
+            })
+            .onDisappear { viewModel.id = "" }
+            .onAppear { tabbarHidden.wrappedValue = true }
+            .navigationBarColor(.orange1, textColor: .white)
+            .navigationBarBackButtonHidden()
     }
 }
--- iOS/Sources/Scene/FeedDetail/FeedDetailViewModel.swift
+++ iOS/Sources/Scene/FeedDetail/FeedDetailViewModel.swift
@@ -6,7 +6,7 @@ import Service
 class FeedDetailViewModel: ObservableObject {
     @Published var id: String = ""
     @Published var comment: String = ""
-    @Published var heartIsClick: Bool = false
+    @Published var heartCount: Int = 0
     @Published var feedDetail: FeedDetail = .init(
         id: "",
         profileImageUrl: "",
@@ -16,6 +16,7 @@ class FeedDetailViewModel: ObservableObject {
         created: Date(),
         category: "",
         heartCount: 0,
+        heartState: false,
         commentCount: 0,
         favorite: false,
         images: [""],
@@ -23,9 +24,23 @@ class FeedDetailViewModel: ObservableObject {
     )
 
     private let fetchFeedDetailUseCase: FetchFeedDetailUseCase
+    private let likeUseCase: LikeUseCase
+    private let unLikeUseCase: UnLikeUseCase
+    private let favoriteUseCase: FavoriteUseCase
+    private let unFavoriteUseCase: UnFavoriteUseCase
 
-    init(fetchFeedDetailUseCase: FetchFeedDetailUseCase) {
+    init(
+        fetchFeedDetailUseCase: FetchFeedDetailUseCase,
+        likeUseCase: LikeUseCase,
+        unLikeUseCase: UnLikeUseCase,
+        favoriteUseCase: FavoriteUseCase,
+        unFavoriteUseCase: UnFavoriteUseCase
+    ) {
         self.fetchFeedDetailUseCase = fetchFeedDetailUseCase
+        self.likeUseCase = likeUseCase
+        self.unLikeUseCase = unLikeUseCase
+        self.favoriteUseCase = favoriteUseCase
+        self.unFavoriteUseCase = unFavoriteUseCase
     }
 
     private var bag = Set<AnyCancellable>()
@@ -38,10 +53,58 @@ class FeedDetailViewModel: ObservableObject {
             }
             .sink { [weak self] feedDetail in
                 self?.feedDetail = feedDetail
+                self?.heartCount = 0
             }
             .store(in: &bag)
     }
 
     func like() {
+        if feedDetail.heartState {
+            self.unLikeUseCase.excute(feedId: id)
+                .catch { error -> Empty<Void, Never> in
+                    print(error)
+                    return .init()
+                }
+                .sink { [weak self] in
+                    self?.feedDetail.heartState.toggle()
+                    self?.feedDetail.heartCount -= 1
+                }
+                .store(in: &bag)
+        } else {
+            self.likeUseCase.excute(feedId: id)
+                .catch { error -> Empty<Void, Never> in
+                    print(error)
+                    return .init()
+                }
+                .sink { [weak self] in
+                    self?.feedDetail.heartState.toggle()
+                    self?.feedDetail.heartCount += 1
+                }
+                .store(in: &bag)
+        }
+    }
+
+    func bookmark() {
+        if feedDetail.favorite {
+            self.unFavoriteUseCase.excute(feedId: id)
+                .catch { error -> Empty<Void, Never> in
+                    print(error)
+                    return .init()
+                }
+                .sink { [weak self] in
+                    self?.feedDetail.favorite.toggle()
+                }
+                .store(in: &bag)
+        } else {
+            self.favoriteUseCase.excute(feedId: id)
+                .catch { error -> Empty<Void, Never> in
+                    print(error)
+                    return .init()
+                }
+                .sink { [weak self] in
+                    self?.feedDetail.favorite.toggle()
+                }
+                .store(in: &bag)
+        }
     }
 }
