aac7cb548ff3a283be2a0892c440959cf9818b20
rlarldud1234 <rlarldud6482@naver.com>
2022-12-25 15:42:00 +0900

    ⚡️ :: 검색 기능 구현

--- iOS/Sources/Scene/Search/SearchView.swift
+++ iOS/Sources/Scene/Search/SearchView.swift
@@ -2,34 +2,39 @@ import SwiftUI
 
 struct SearchView: View {
     @StateObject var viewModel: SearchViewModel
+    @Environment(\.tabbarHidden) var tabbarHidden
+    let feedDetailView: FeedDetailView
 
     var body: some View {
         NavigationView {
             ZStack {
                 if !viewModel.searchList.isEmpty {
-                    VStack(spacing: 0) {
+                    VStack(
+                        alignment: .leading,
+                        spacing: 0
+                    ) {
                         Spacer()
                             .frame(height: 18)
-                        HStack {
-                            Text("검색어")
-                                .font(.button500)
-                                .foregroundColor(.gray1)
-                            Spacer()
+                        Text("검색어")
+                            .font(.button500)
+                            .foregroundColor(.gray1)
+                            .padding(.leading, 33)
+                        LazyVStack {
+                            ForEach(viewModel.searchList, id: \.feedId) { search in
+                                NavigationLink(destination: self.feedDetailView) {
+                                    SearchListCell(
+                                        search: search.title,
+                                        searchText: $viewModel.searchText
+                                    )
+                                    .onDisappear {
+                                        viewModel.id = search.feedId
+                                    }
+                                }
+                            }
                         }
-                        .padding(.leading, 33)
-                        List(
-                            viewModel.searchList,
-                            id: \.self
-                        ) {
-                            SearchListCell(
-                                search: $0,
-                                searchText: $viewModel.searchText
-                            )
-                        }
-                        .listStyle(.inset)
+                        Spacer()
                     }
-                }
-                if viewModel.searchText.isEmpty {
+                } else {
                     VStack {
                         Text("최근 검색 내역이 없습니다.")
                             .font(.body400)
@@ -39,13 +44,17 @@ struct SearchView: View {
                     }
                 }
             }
+            .onDisappear { feedDetailView.viewModel.id = viewModel.id }
+            .onAppear { tabbarHidden.wrappedValue = false }
             .navigationBarTitleDisplayMode(.inline)
+            .onChange(of: viewModel.searchText) { _ in
+                viewModel.searchList = []
+                viewModel.fetchSearchList()
+            }
             .toolbar {
                 ToolbarItem(placement: .principal) {
                     SearchBar(
-                        action: {
-                            print("search")
-                        },
+                        action: viewModel.fetchSearchList,
                         text: $viewModel.searchText
                     )
                 }
@@ -56,12 +65,5 @@ struct SearchView: View {
             )
         }
         .hideKeyboard()
-    }
-}
-
-struct SearchView_Previews: PreviewProvider {
-    static var previews: some View {
-        let viewModel = SearchViewModel()
-        SearchView(viewModel: viewModel)
     }
 }
--- iOS/Sources/Scene/Search/SearchViewModel.swift
+++ iOS/Sources/Scene/Search/SearchViewModel.swift
@@ -1,6 +1,30 @@
 import Foundation
 
+import Combine
+import Service
+
 class SearchViewModel: ObservableObject {
     @Published var searchText: String = ""
-    @Published var searchList: [String] = []
+    @Published var searchList: [Search] = []
+    @Published var id: String = ""
+
+    private let searchUseCase: SearchUseCase
+
+    init(searchUseCase: SearchUseCase) {
+        self.searchUseCase = searchUseCase
+    }
+
+    private var bag = Set<AnyCancellable>()
+
+    func fetchSearchList() {
+        self.searchUseCase.excute(keyword: searchText)
+            .catch { error -> Empty<[Search], Never> in
+                print(error)
+                return .init()
+            }
+            .sink { [weak self] in
+                self?.searchList = $0
+            }
+            .store(in: &bag)
+    }
 }
